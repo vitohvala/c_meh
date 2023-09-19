4a44e619d19c0a4c4b845d8b819ef98c2282ce1c
vitohvala <hvalavitalisu@gmail.com>
2023-09-19 22:09:11 +0200

    better snake

--- snake/libtastatura.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TASTATURA_H__
-#define __TASTATURA_H__
-
-int kbhit(void);
-int getch(void);
-
-#endif
--- snake/snake.c
+++ snake/snake.c
@@ -1,95 +1,108 @@
+#include <asm-generic/ioctls.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
 #include <assert.h>
-#include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <sys/ioctl.h>
 
-#define ROW 50
-#define COL 25
 
-#ifdef __WIN32__
+#define ROW 20
+#define COL 50
 
-#include <conio.h>
-#define CLEAR "@cls"
+typedef enum {
+    LEFT = 1,
+    RIGHT,
+    UP,
+    DOWN,
+    EXIT
+}DIRECTION;
 
-#else
+typedef struct {
+    int x[ROW * COL], y[ROW * COL];
+    int snake_size;
+    int score;
+} SNAKE;
 
-#define CLEAR "clear"
-#include "libtastatura.h"
+static struct termios old_termios;
 
-#endif
-
-int msleep(long tms){
-    struct timespec ts;
-    int ret;
-
-    if (tms < 0)
-    {
-        errno = EINVAL;
-        return -1;
-    }
-
-    ts.tv_sec = tms / 1000;
-    ts.tv_nsec = (tms % 1000) * 1000000;
-
-    do {
-        ret = nanosleep(&ts, &ts);
-    } while (ret && errno == EINTR);
-
-    return ret;
+void clear(void){
+    printf("\x1b[2J");
+    printf("\x1b[H");
 }
 
-void crtaj(int x_voc, int y_voc, int score, int tailX[], int tailY[], int k, int x, int y){
-    uint16_t i, j;
-    system(CLEAR);
-    for(i = 0; i < COL + 1; i++){
-        for(j = 0; j < ROW + 1; j++){
-            if(i == 0) printf("_");
-            else if(j == 0 || j == ROW) printf("|");
-            else if(i == COL) printf("-");
-            else if(i == x && j == y) printf("S");
-            else if(x_voc == i && y_voc == j)printf("\033[38;5;208mX\033[0m");
+void reset_terminal(){
+    fflush(stdout);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+    printf("\x1b[?25h");
+}
+
+void terminal_config(){
+    int res = 0;
+    printf("\x1b[?25l");
+    res = tcgetattr(STDIN_FILENO, &old_termios);
+    assert(res == 0);
+    struct termios new_termios = old_termios;
+ 
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    new_termios.c_cc[VMIN] = 1;
+    new_termios.c_cc[VTIME] =  1;
+
+    res = tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+    assert(res == 0);
+
+    atexit(reset_terminal);
+}
+
+bool kbhit(void){
+    int btw;
+    ioctl(0, FIONREAD, &btw);
+    return btw > 0;
+}
+
+void draw(SNAKE *snake, int x_fruit, int y_fruit){
+    int  sem = 0;
+    printf("Press q to quit, W, A, S, D to move\r\n");
+    for(int i = 0; i < ROW; i++){
+        for(int j = 0; j < COL; j++){
+            if(i == 0 || i == ROW - 1) printf("-");
+            else if(j == 0 || j == COL - 1) printf("|");
+            else if(j == y_fruit && i == x_fruit) printf("\033[38;5;119mX\033[0m");
             else {
-                int semafor = 0;
-                for(int w = 0; w < k; w++){
-                    if(tailX[w] == i && tailY[w] == j){
-                        printf("S");
-                        semafor = 1;
+                sem = 0;
+                for(int k = 0; k < snake->snake_size; k++){
+                    if(snake->x[k] == j && snake->y[k] == i){
+                        printf("%s", (k == 0) ? "\033[38;5;167mS\033[0m" : "S");
+                        sem = 1;
                     }
                 }
-                if(semafor == 0) printf(" ");
+                if(sem == 0) printf(" ");
             }
         }
-        printf("\n");
-    }
-    printf("score: [%d]\n", score);
+        printf("\r\n");
+    } 
+    printf("score [%d]\r\n", snake->score);
 }
 
-bool provera(int x, int y, int tx[], int ty[], int k){
-    int i, br = 0;
-    for(i = 0; i < k; i++)
-        if(x != tx[i] && ty[i] != y) br++;
-    return (br == k) ? true : false;
+int check_pos(int *x, int *y, SNAKE *snake){
+    if(*x < 1 || *x > COL - 1) return 1;
+    if(*y < 1 || *y > ROW - 1) return 1;
+    for(int i = 0; i < snake->snake_size; i++){
+        if(*x == snake->x[i] && *y == snake->y[i]) return 1;
+    }
+    return 0;
 }
 
-
-void voce(int *poz, int *poz2, int tailX[], int tailY[], int k, int x, int y){
-    srand(time(NULL));
-    int dd = COL - 2;
-    int yy = ROW - 2;
-    int x_poz, y_poz;
-opet:
-     x_poz = rand() % dd + 1;
-     y_poz = rand() % yy + 1;
-    if(provera(x_poz, y_poz, tailX, tailY, k) && (x != x_poz && y != y_poz)) {
-        *poz = x_poz;
-        *poz2 = y_poz;
-    }
-    else goto opet;
+void put_fruit(SNAKE *snake, int *x_fruit, int *y_fruit){
+    srand(time(0));
+    do {
+        *x_fruit = (rand()%COL - 2) + 1;
+        *y_fruit = (rand()%ROW - 2) + 1;
+    } while(check_pos(x_fruit, y_fruit, snake));
 }
 
 void unesi_score(int score){
@@ -118,93 +131,108 @@ int maxx(){
     fclose(dat);
     return max;
 }
+
+void move_snake(SNAKE *snake, int strana){
+    if(strana){
+        for(int i = snake->snake_size - 1; i > 0; i--){
+            snake->x[i] = snake->x[i - 1];
+            snake->y[i] = snake->y[i - 1];
+        }
+    }
+    switch (strana) {
+        case LEFT:
+            snake->x[0]--;
+            break;
+        case RIGHT:
+            snake->x[0]++;
+            break;
+        case UP:
+            snake->y[0]--;
+            break;
+        case DOWN:
+            snake->y[0]++;
+            break;
+    }
+    if(strana == DOWN && snake->y[0] >= ROW) snake->y[0] = 1;
+    if(strana == UP && snake->y[0] < 0) snake->y[0] = ROW - 2;
+    if(strana == LEFT && snake->x[0] < 0) snake->x[0] = COL - 2;
+    if(strana == RIGHT && snake->x[0] >= COL) snake->x[0] = 1;
+}
+
+void input_control(int *strana){
+    int c = getchar();
+    switch(c){
+        case 'A':
+        case 'a':
+            if(*strana != RIGHT) *strana = LEFT;
+            break;
+        case 's':
+        case 'S':
+            if(*strana != UP) *strana = DOWN;
+            break;
+        case 'D':
+        case 'd':
+            if(*strana != LEFT) *strana = RIGHT;
+            break;
+        case 'W':
+        case 'w':
+            if(*strana != DOWN) *strana = UP;
+            break;
+        case 'Q':
+        case 'q':
+            (*strana) = EXIT;
+            break;
+    }
+}
+
+void collision_det(uint8_t *end, SNAKE *snake, int *x, int *y){
+    if(snake->x[0] == *x && snake->y[0] == *y) {
+        put_fruit(snake, x, y);
+        snake->score += (snake->score % 30 == 0 && snake->score != 0) ? 10 : 5;
+        snake->snake_size++;
+    }
+    for(int i = 2; i < snake->snake_size; i++){
+        if(snake->x[0] == snake->x[i] && snake->y[0] == snake->y[i]) *end = 0;
+    }
+}
+
+void init(SNAKE *snake){
+    snake->x[0] = COL / 2 - 1;
+    snake->y[0] = ROW / 2 - 1;
+    snake->snake_size = 6; 
+    for(int i = 1; i < snake->snake_size; i++){
+        snake->y[i] = snake->y[i - 1] - 1;
+        snake->x[i] = COL / 2 - 1;
+
+    }
+    snake->score = 0;
+}
 int main(void){
+    int strana = 0, x_fruit = -1, y_fruit = -1;
+    uint8_t end = 1;
+    SNAKE snake;
 
-    int tailX[10000], tailY[10000];
-
-    printf("\033[?25l");
-    int poz = 0, x_voc, y_voc, end  = 1,  k = 6,
-        score = 0, c = 0, brzina = 70;
-
-    int x, y;
-
-    x = COL / 2;
-    y = ROW / 2;
-
-    voce(&x_voc, &y_voc, tailX, tailY, k, x, y);
+    init(&snake);
+    put_fruit(&snake, &y_fruit, &x_fruit);
+    terminal_config();
+    
     while(end){
-        crtaj(x_voc, y_voc,score, tailX, tailY, k, x, y);
-        if(kbhit()) c = getch();
-        if((c == 'A' || c == 'a') && poz != 3) {
-            poz = 1;
+        clear();
+        draw(&snake, x_fruit, y_fruit);
+        if(kbhit()){
+            input_control(&strana);
         }
-        else if((c == 'S' || c == 's') && poz != 4) {
-            poz = 2;
-        }
-        else if((c == 'd' || c == 'D') && poz != 1) {
-            poz = 3;
-        }
-        else if((c == 'w' || c == 'W') && poz != 2) {
-            poz = 4;
-        }
-        else if(c == 'q') end = 0;
-        switch(poz){
-            case 1:
-                y--;
-                if(y < 1) y = ROW - 1;
-                break;
-            case 2:
-                x++;
-                if(x > COL - 1) x = 1;
-                break;
-            case 3:
-                y++;
-                if(y > ROW - 1) y = 1;
-                break;
-            case 4:
-                x--;
-                if(x < 1) x = COL - 1;
-                break;
-            default:
-                break;
-        }
-        if(k > 6)
-            for(int i = 0; i < k; i++){
-                if(tailX[i] == x && tailY[i] == y)
-                    end = 0;
-            }
-        int prevX = tailX[0];
-        int prevY = tailY[0];
-        int prev2X, prev2Y;
-        tailX[0] = x;
-        tailY[0] = y;
-        for (int i = 1; i < k; i++){
-            prev2X = tailX[i];
-            prev2Y = tailY[i];
-            tailX[i] = prevX;
-            tailY[i] = prevY;
-            prevX = prev2X;
-            prevY = prev2Y;
-        }
-
-        if(x == x_voc && y == y_voc){
-            score += 5;
-            k++;
-            voce(&x_voc, &y_voc, tailX, tailY, k, x, y);
-        }
-        (poz == 1 || poz == 3 ) ? msleep(brzina) : msleep(brzina * 2);
-
+        if(strana == EXIT) end = 0;
+        move_snake(&snake, strana);
+        collision_det(&end, &snake, &y_fruit, &x_fruit);
+        usleep(150000); 
     }
-    system("clear");
-    unesi_score(score);
     int max = maxx();
-    if(max == score) printf("Novi rekord: %d!\n", score);
-    else{
-        printf("Maksimalan score: %d\n", max);
-        printf("Trenutni score: %d\n", score);
+    if(snake.score >= max) printf("New high score: %d\r\n", snake.score);
+    else {
+        printf("Your score: %d\r\n", snake.score);
+        printf("Max score: %d\r\n", max);
     }
-    printf("\033[?25h");
-    printf("GAME OVER\n");
-
+    unesi_score(snake.score);
     return 0;
 }
